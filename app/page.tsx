d49507b37b009c66b551dfa2c72e75c222ec84f7
nikitamalinov <nikita@malinovsky.net>
2024-02-27 22:44:40 -0800

    second commit

---
+++ app/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center pt-5">
+      <nav>Create Nav Bar</nav>
+      <h1>Fix Bugs Automatically</h1>
+      <h1>Fix Bugs Using AI</h1>
+      <h1>Automatic PRs for old bugs</h1>
+      <div>Create Footer</div>
+    </main>
+  );
+}
