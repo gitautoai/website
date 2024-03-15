8555dfab38dfbbd5b6deaf604b688202d0f87cc5
nikitamalinov <nikita@malinovsky.net>
2024-03-15 14:13:40 -0700

    removing search params

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ import Pricing from "@/components/HomePage/Pricing";
 import { motion } from "framer-motion";
 
 // Analytics
-import { usePathname, useSearchParams } from "next/navigation";
+import { usePathname } from "next/navigation";
 import { usePostHog } from "posthog-js/react";
 
 const childVariants = {
@@ -47,20 +47,16 @@ export default function Home() {
 
   // Analytics
   const pathname = usePathname();
-  const searchParams = useSearchParams();
   const posthog = usePostHog();
 
   useEffect(() => {
     if (pathname && posthog) {
       let url = window.origin + pathname;
-      if (searchParams.toString()) {
-        url = url + `?${searchParams.toString()}`;
-      }
       posthog.capture("$pageview", {
         $current_url: url,
       });
     }
-  }, [pathname, searchParams, posthog]);
+  }, [pathname, posthog]);
 
   // const stripe = useStripe();
   // const elements = useElements();
