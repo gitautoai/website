df9a50bbb6a19c472999e62e436cfcf4c43edc0f
nikitamalinov <nikita@malinovsky.net>
2024-03-19 17:24:59 -0700

    small fixes

--- components/HomePage/Features.tsx
+++ components/HomePage/Features.tsx
@@ -4,7 +4,7 @@ export default function Features() {
     <div className="bg-white text-black w-[100vw] flex flex-col py-16">
       <div className="flex flex-col items-center">
         <h2 className="text-center text-3xl">Let AI do the work for you</h2>
-        <div className=" w-[95vw] sm:w-[80vw] xl:w-[1000px] 2xl:w-[1250px] mt-16">
+        <div className=" w-[95vw] sm:w-[80vw] lg:w-[1000px] 2xl:w-[1250px] mt-16 mx-5">
           <div className="flex flex-col gap-10 lg:flex-row justify-between items-center">
             <div className="flex flex-col gap-2 min-w-[200px] lg:max-w-[200px] xxl:max-w-auto">
               <span className="text-3xl">Feature Requests</span>
@@ -15,7 +15,7 @@ export default function Features() {
               width={907}
               height={252}
               alt="Feature Request"
-              className="p-5 bg-light rounded-lg"
+              className="p-2 sm:p-3 lg:p-4 bg-light rounded-lg"
             />
           </div>
           <div className="flex flex-col gap-10 lg:flex-row justify-between items-center mt-10">
@@ -28,7 +28,7 @@ export default function Features() {
               width={907}
               height={252}
               alt="Feature Request"
-              className="p-5 bg-light rounded-lg"
+              className="p-2 sm:p-3 lg:p-4 bg-light rounded-lg"
             />
           </div>
           {/* Not yet a feature */}
