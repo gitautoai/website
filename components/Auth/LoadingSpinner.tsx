34ba1a3f5ea410836992294017e905a59276664b
nikitamalinov <nikita@malinovsky.net>
2024-03-27 00:36:01 -0700

    .

--- components/Auth/LoadingSpinner.tsx
+++ components/Auth/LoadingSpinner.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export default function LoadingSpinner() {
   return (
-    <div className="flex justify-center items-center h-[calc(100vh-232px)] ">
+    <div className="flex justify-center items-center h-[calc(100vh-232px)] bg-light">
       <div className="relative flex items-center">
         <Spinner
           thickness="4px"
