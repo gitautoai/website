a6b95bc1a472816d30ac57e1fe18e19efcb7529f
Hiroshi Nishio <4620828+hiroshinishio@users.noreply.github.com>
2024-07-17 23:32:19 +0900

    Refactor some CSS

--- components/HomePage/ValueProp.tsx
+++ components/HomePage/ValueProp.tsx
@@ -4,22 +4,20 @@ const ValueProp = () => {
   return (
     <div
       id="top"
-      className="h-screen flex flex-col items-center justify-center gap-8 md:gap-6 lg:gap-8 xl:gap-10"
+      className="h-screen flex flex-col items-center justify-center gap-4 sm:gap-2 md:gap-4 text-center"
     >
-      <div className="flex flex-col gap-4 text-center">
-        <h1 className="text-4xl sm:text-4xl md:text-6xl font-semibold bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 bg-clip-text text-transparent pb-1 mt-10 sm:mt-14 md:mt-10">
-          Let your AI Coding Agent writes code.
-          {/* Let your AI coding agent resolve your issues.
+      <h1 className="text-4xl sm:text-4xl md:text-6xl font-semibold bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 bg-clip-text text-transparent pb-1 mt-10 sm:mt-14 md:mt-10">
+        Let your AI Coding Agent writes code.
+        {/* Let your AI coding agent resolve your issues.
             Let your AI coding agent fix your bugs.
             Let your AI coding agent develop your features.
             Hire your AI coding agent. */}
-        </h1>
-        <p className="text-lg sm:text-lg md:text-2xl lg:text-3xl">
-          GitAuto automatically writes code and creates pull requests daily for your issues on
-          GitHub, fixing more bugs and shipping more features.
-        </p>
-        <InstallButton text="Get Started for Free" />
-      </div>
+      </h1>
+      <p className="text-lg sm:text-lg md:text-2xl lg:text-3xl">
+        GitAuto automatically writes code and creates pull requests daily for your issues on GitHub,
+        fixing more bugs and shipping more features.
+      </p>
+      <InstallButton text="Get Started for Free" />
     </div>
   );
 };
