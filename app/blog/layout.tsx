b467171bf4de190f950682cd3e45b519bf88ff0e
Hiroshi Nishio <hnishio0105@gmail.com>
2025-01-05 16:21:47 +0900

    Add canonical and og:url to each blog post page metadata

--- app/blog/layout.tsx
+++ app/blog/layout.tsx
@@ -1,23 +1,4 @@
-import { Metadata } from "next";
-import { headers } from "next/headers";
 import InstallButton from "@/components/Button/Install";
-
-// export async function generateMetadata(): Promise<Metadata> {
-//   const headersList = await headers();
-//   const referer = headersList.get("referer");
-//   const url = new URL(referer || "");
-//   const pathname = url.pathname;
-//   console.log({ headersList });
-
-//   return {
-//     alternates: {
-//       canonical: pathname,
-//     },
-//     openGraph: {
-//       url: pathname,
-//     },
-//   };
-// }
 
 /**
  * @see https://nextjs.org/docs/app/building-your-application/routing/pages-and-layouts
