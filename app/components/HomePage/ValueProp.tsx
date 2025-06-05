23dc63eb266d91ce0894107717d10ad65b21daac
Hiroshi Nishio <hnishio0105@gmail.com>
2025-06-05 16:14:17 -0700

    .

--- app/components/HomePage/ValueProp.tsx
+++ app/components/HomePage/ValueProp.tsx
@@ -4,17 +4,21 @@ const ValueProp = () => {
   return (
     <div
       id="top"
-      className="min-h-screen h-screen flex flex-col items-center justify-center gap-4 sm:gap-2 md:gap-4 text-center"
+      className="min-h-screen h-screen flex flex-col items-center justify-center gap-2 sm:gap-2 md:gap-4 text-center"
     >
-      <h1 className="text-4xl sm:text-4xl md:text-6xl font-semibold bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 bg-clip-text text-transparent pb-1 mt-10 sm:mt-14 md:mt-10 sm:w-9/12 md:w-8/12 h-20 sm:h-20 md:h-32">
-        GitAuto is a QA agent for GitHub
+      <h1 className="text-4xl sm:text-4xl md:text-6xl font-bold bg-gradient-to-r from-purple-600 via-pink-500 to-red-500 bg-clip-text text-transparent pb-0 mt-0 mb-0 sm:mt-14 md:mt-10 sm:w-12/12 md:w-11/12 h-32 sm:h-20 md:h-32">
+        Go from 0% to 90%
+        <br className="hidden sm:block" />
+        Test Coverage Quickly
       </h1>
-      <p className="text-lg sm:text-lg md:text-2xl lg:text-3xl">
-        GitAuto creates unit test PRs, runs the tests, and fixes failures if any, to boost your test
-        coverage from 0% to 90% – and keeps it there.
+      <p className="text-lg sm:text-lg md:text-2xl lg:text-3xl font-medium">
+        GitAuto automatically writes, runs, and fixes your unit tests,
+        <br className="hidden sm:block" /> so you can keep shipping confidently.
       </p>
-      <InstallButton text="Get Started for Free" />
-      <p className="text-base sm:text-sm md:text-base lg:text-lg">It requires GitHub sign-in.</p>
+      <InstallButton text="Start Free – Add to GitHub" />
+      <p className="text-sm sm:text-xs md:text-sm lg:text-base text-gray-500">
+        No credit card required. Get started in seconds.
+      </p>
     </div>
   );
 };
