af0f979141709d1252767589f3cab073cd9f1034
Hiroshi Nishio <hnishio0105@gmail.com>
2025-06-19 12:04:45 -0700

    .

---
+++ types/prismjs.d.ts
@@ -0,0 +1,7 @@
+// PrismJS component modules type declarations
+declare module "prismjs/components/prism-bash";
+declare module "prismjs/components/prism-javascript";
+declare module "prismjs/components/prism-json";
+declare module "prismjs/components/prism-typescript";
+declare module "prismjs/components/prism-yaml";
+declare module "prismjs/themes/prism-tomorrow.css";
