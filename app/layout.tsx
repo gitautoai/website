75da14fe6c80e3fd2d89768bdec6b000b3b02c5d
Hiroshi Nishio <hnishio0105@gmail.com>
2025-09-04 16:57:42 +0900

    Remove Apollo tracking script to fix unhandled promise rejection on blog page

--- app/layout.tsx
+++ app/layout.tsx
@@ -36,11 +36,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationJsonLd) }}
         />
       </head>
-      {isPrd && (
-        <Script id="apollo-script">
-          {`function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,o.onload=function(){window.trackingFunctions.onLoad({appId:"6631c123b9befb01c76f5219"})},document.head.appendChild(o)}initApollo();`}
-        </Script>
-      )}
       <PostHogWrapper>
         <body className={`${inter.className} w-full min-h-screen text-base sm:text-sm md:text-xl`}>
           <Suspense>
